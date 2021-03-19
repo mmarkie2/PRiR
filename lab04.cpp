6f57dd7105df209b4946ccbab98d7295641b1368
mm <maciej.markiewicz10@gmail.com>
2021-03-19 15:32:18 +0100

    clion

--- lab04.cpp
+++ lab04.cpp
@@ -6,6 +6,9 @@
 #include <mutex>
 #include <vector>
 #include <string>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
 using namespace std;
 
 const int OPOZNIENIE = 100;
@@ -125,15 +128,15 @@ public:
 
     void operator()() {
 
-            licznikMutex.lock();
+        licznikMutex.lock();
         for (int i = 0; i < ZNAKOW_DO_ZAPISANIA; ++i) {
 
-                lancuch[i] = znak;
+            lancuch[i] = znak;
 
             this_thread::sleep_for(chrono::microseconds(OPOZNIENIE));
 
         }
-            licznikMutex.unlock();
+        licznikMutex.unlock();
 
     }
 };
@@ -150,11 +153,70 @@ void l4z4() {
     for (thread &w: watki) {
         w.join();
     }
-    cout << lancuch<<endl;
+    cout << lancuch << endl;
 }
 
 
 //dodatkowe tablica mutexow, zmienna globalna licznik
+const int arrSize = 100;
+
+int modifiedNumbers = 0;
+mutex numbersMutex[arrSize];
+
+class Thread5 {
+private:
+
+    int *numbers;
+int id;
+int &score;
+public:
+    Thread5(int _id, int *_numbers, int &_score) : id(_id), numbers(_numbers), score(_score) {  }
+
+    void operator()() {
+        while (modifiedNumbers < arrSize) {
+
+
+            for (int i = 0; i < 2; ++i) {
+                int pos1 = rand() % arrSize;
+
+
+                numbersMutex[pos1].lock();
+                if (numbers[pos1] > 0) {
+                    score += numbers[pos1];
+                    numbers[pos1] = 0;
+                    modifiedNumbers++;
+                }
+                numbersMutex[pos1].unlock();
+            }
+            cout <<id<<"  score:  "<<score<< endl;
+            this_thread::sleep_for(chrono::microseconds(score*10));
+
+        }
+    }
+};
+
 void l4zDodatkowe() {
 
+    int *numbers=new int[arrSize];
+    srand(time(NULL));
+    int sum=0;
+    for (int i = 0; i < arrSize; ++i) {
+        numbers[i] =  1;
+        sum+=numbers[i];
+    }
+    cout<<"suma tablicy: "<<sum << endl;
+    int score1=0;    int score2=0;
+    thread w1(Thread5(1,numbers,score1 ));
+    thread w2(Thread5(2,numbers,score2));
+    w1.join();
+    w2.join();
+
+    sum=0;
+    for (int i = 0; i < arrSize; ++i) {
+
+        sum+=numbers[i];
+    }
+    cout<<"suma tablicy po: "<<sum << endl;
+    cout<<"wynik watku 1: "<<score1 <<"  wynik watku 2: "<<score2 << "  suma  "<<score1 +score2<<endl;
+    delete[] numbers;
 }

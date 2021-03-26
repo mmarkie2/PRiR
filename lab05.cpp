60df6624cf2e69b46ca844d219cbfbd98115b639
mm <maciej.markiewicz10@gmail.com>
2021-03-26 13:04:58 +0100

    clion

--- lab05.cpp
+++ lab05.cpp
@@ -4,9 +4,13 @@
 
 #include <iostream>
 #include <mutex>
+#include <random>
 
 
 using namespace std;
+
+const int CZAS_PRACY=5;
+
 
 template <typename TypDanych>
 class Monitor
@@ -17,11 +21,69 @@ private:
     mutex mutexDane;
     condition_variable warunekSaNoweDane;
     condition_variable warunekZabranoDane;
+
+public:
+    Monitor()
+    {
+        saDane=false;
+
+    }
+    void zapisz(TypDanych noweDane)
+    {
+       unique_lock<mutex> blokada(mutexDane);
+       saDane=true;
+       dane=noweDane;
+       warunekSaNoweDane.notify_one();
+    }
+    TypDanych odczytaj()
+    {
+        unique_lock<mutex> blokada(mutexDane);
+
+        if(!saDane)
+        {
+            warunekSaNoweDane.wait(blokada);
+        }
+        saDane=false;
+        warunekZabranoDane.notify_one();
+        return  dane;
+    }
 };
+
+class  Producent
+{
+private:
+    int czas;
+    Monitor<char>& pojemnik;
+    char biezacyZnak;
+
+    minstd_rand generator;
+    uniform_int_distribution<char> rozklad;
+
+public:
+    Producent(Monitor <char>& _pojemnik):
+    czas(0), pojemnik(_pojemnik), biezacyZnak('a'),
+    generator(random_device()()),rozklad('a','z')
+    {
+
+    }
+
+    void operator()()
+    {
+        while (czas<CZAS_PRACY)
+        {
+            pojemnik.zapisz(biezacyZnak);
+            biezacyZnak=(biezacyZnak+1-97)%25-97;
+            this_thread::sleep_for(chrono::seconds(1));
+            ++czas;
+        }
+        pojemnik.zapisz(EOF);
+    }
+};
+
 void l5z1()
 
 {
-const int CZAS_PRACY=5;
+
 
 }
 void l5z2()

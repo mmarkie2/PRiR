224afd653022eb8ead5a1107b80b78839135da48
mm <maciej.markiewicz10@gmail.com>
2021-04-16 17:17:36 +0200

    clion

--- lab06.cpp
+++ lab06.cpp
@@ -5,19 +5,28 @@
 #include <string>
 #include <random>
 #include <thread>
+
+#include <vector>
 #include "Semafor.h"
+
 using namespace std;
 
 class Peron;
 
+class Stacja;
+
 class Pociag {
 private:
     string m_nazwa;
-    Peron &peron;
-    Peron &peronZapasowy;
+public:
+    const string &getMNazwa() const;
+
+private:
+    Stacja &stacja;
+
 
 public:
-    Pociag(string n, Peron &p1, Peron &p2);
+    Pociag(string n, Stacja &stacja);
 
     void operator()();
 
@@ -29,15 +38,26 @@ public:
 class Peron {
 private:
     string nazwa;
-    bool pusty;
-    Semafor semafor;
 public:
-    Peron(string n) : nazwa(n), pusty(true),semafor(1) {
+    const string &getNazwa() const {
+        return nazwa;
+    }
+
+private:
+    bool pusty;
+    Pociag *pociag = nullptr;
+public:
+    Pociag *getPociag() const {
+        return pociag;
+    }
+
+public:
+    Peron(string n) : nazwa(n), pusty(true) {
 
     }
 
     void przyjazd(Pociag &p) {
-        semafor.czekaj();
+        pociag = &p;
         if (pusty) {
             string info;
             info.append("Pociąg ");
@@ -68,36 +88,86 @@ public:
         info.append("\n ");
         cout << info;
         pusty = true;
-        semafor.sygnalizuj();
+        pociag = nullptr;
     }
 
     bool czyPusty() {
         return pusty;
     }
+
+
+};
+
+class Stacja {
+    vector<Peron>& perony;
+    Semafor semafor;
+    mutex mutexPerony;
+public :
+    explicit Stacja(vector<Peron>& _perony) : perony(_perony), semafor(_perony.size()) {
+
+    }
+
+    bool czyWolnyPeron() {
+        return semafor.wartosc() > 0;
+    }
+void wyswietlPerony()
+{
+    string s="";
+    for ( Peron& peron:perony) {
+       s+="peron: "+peron.getNazwa()+ "  ";
+        if (peron.getPociag()!= nullptr ) {
+          s+=   peron.getPociag()->nazwa();
+        }else
+        {
+            s+= "brak   ";
+        }
+
+    }
+    cout<<s+", wolne perony: "+to_string(semafor.wartosc())+"  \n";
+
+
+}
+    void przyjazd(Pociag &pociag) {
+        semafor.czekaj();
+        unique_lock<mutex> blokadaLicznika(mutexPerony);
+        for (Peron &peron:perony) {
+            if (peron.czyPusty()) {
+                peron.przyjazd(pociag);
+                wyswietlPerony();
+                return;
+            }
+
+        }
+        perony[0].przyjazd(pociag);
+        wyswietlPerony();
+    }
+
+    void odjazd(Pociag &pociag) {
+
+        for (Peron& peron:perony) {
+            if (peron.getPociag()!= nullptr && peron.getPociag()->nazwa() == pociag.nazwa()) {
+                peron.odjazd(pociag);
+                break;
+            }
+        }
+        semafor.sygnalizuj();
+    }
+
 };
 
 minstd_rand generator;
 uniform_int_distribution<int> zakres(0, 500);
 
 
-Pociag::Pociag(string n, Peron &p1, Peron &p2)
-        : m_nazwa(n), peron(p1), peronZapasowy(p2) {
-
-}
-
 void Pociag::operator()() {
     for (int i = 0; i < 3; i++) {
         this_thread::sleep_for(chrono::milliseconds(zakres(generator)));
-        if (peron.czyPusty()) {
-            peron.przyjazd(*this);
-            this_thread::sleep_for(chrono::milliseconds(zakres(generator)));
-            peron.odjazd(*this);
 
-        } else {
-            peronZapasowy.przyjazd(*this);
+            stacja.przyjazd(*this);
             this_thread::sleep_for(chrono::milliseconds(zakres(generator)));
-            peronZapasowy.odjazd(*this);
-        }
+            stacja.odjazd(*this);
+
+
     }
 }
 
@@ -105,21 +175,34 @@ string Pociag::nazwa() {
     return m_nazwa;
 }
 
+Pociag::Pociag(string n, Stacja &stacja) : m_nazwa(n), stacja(stacja) {
+
+}
+
+const string &Pociag::getMNazwa() const {
+    return m_nazwa;
+}
+
 
 void l6z1() {
-    Peron p1("p1"), p2("p2"), p3("p3");
-    Pociag pociag1("pociag1", p2, p1);
-    Pociag pociag2("pociag2", p2, p3);
-    Pociag pociag3("pociag3", p2, p1);
-    Pociag pociag4("pociag4", p2, p3);
+    vector<Peron> perony;
+    Peron p1("p1"),p2("p2"),p3("p3");
+    perony.push_back( p1);
+    perony.push_back(p2);
+    perony.push_back(p3);
+    Stacja stacja(perony);
 
-    thread w1(pociag1),w2(pociag2),w3(pociag3),w4(pociag4);
+
+    Pociag pociag1("pociag1", stacja);
+    Pociag pociag2("pociag2", stacja);
+    Pociag pociag3("pociag3", stacja);
+    Pociag pociag4("pociag4", stacja);
+
+    thread w1(pociag1), w2(pociag2), w3(pociag3), w4(pociag4);
     w1.join();
     w2.join();
     w3.join();
     w4.join();
-
-
 
 
 }
